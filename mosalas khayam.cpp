9a9adf55ea4f93394f4d678b4fca90d5aea472b6
Fatime pls <mhrsamnbynzhad@gmail.com>
2025-12-12 19:58:02 +0330

    Add files via upload

---
+++ mosalas khayam.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    if( (a*a) +(b*b) == (c*c) || (b*b)+(c*c)==(a*a) || (a*a)+(c*c)==(b*b) )
+    
+    {
+        cout<< "Yes"nss;
+    
+    }
+    
+    else
+    {
+        cout<< "NO"n;
+
+    
+    }
+    return 0 ;
+
+ 
+
+
+}
